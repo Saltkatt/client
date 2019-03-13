3e45b342116c32e8f7a8cb1957193ff70c3fa0f4
Elin <elin.sexton@iths.se>
2019-03-13 17:15:49 +0100

    Added menu in ClientController, links to URL in ContactService

--- client/src/main/java/com/example/client/ClientController.java
+++ client/src/main/java/com/example/client/ClientController.java
@@ -1,31 +1,130 @@
 package com.example.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientController {
 
+    private ContactService conService;
     private Scanner sc;
 
-    //give user choices
-    //1 go to return-service
-    //2 go to default message
+    public ClientController(ContactService conService) {
+        this.conService = conService;
+        sc = new Scanner(System.in);
+    }
+
 
     public void run(){
 
         while(true){
-            //printMenu();
+            mainMenu();
 
             String userInput = sc.nextLine();
 
             switch(userInput) {
-                case "1":
-
+                case "1": getAllBooks(); break;
+                case "2": getBookById(); break;
+                case "3": getBookByTitle(); break;
+                case "4": addBook(); break;
+                case "5": updateBook(); break;
+                case "6": deleteBook(); break;
+                case "7": System.exit(0);
+                default:
+                    System.out.println("Input is not recognised, please try again.");
             }
+            System.out.println("\n");
         }
 
+    }
 
+    private void mainMenu(){
+        List<String> menuOptions = new ArrayList<>();
+        Method[] methods = ContactService.class.getDeclaredMethods();
+        for(Method m: methods){
+            menuOptions.add(m.getName());
+        }
+        Collections.sort(menuOptions); //getDeclaredMethods return them in random order
+        menuOptions.add("exit library");
+        int i = 1;
+        for(String s: menuOptions){
+            System.out.println(i++ + ") " + s);
+        }
+    }
+
+    public void getAllBooks(){
+        String inventory = conService.getAllBooks();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+        List<Book> library = null;
+        try {
+            library = objectMapper.readValue(inventory, typeFactory.constructCollectionType(List.class, Book.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(Book book: library){
+            System.out.println(book);
+        }
+
+    }
+
+    public void getBookById(){
+        System.out.println("Enter book ID: ");
+        Long bookID = sc.nextLong();
+
+        conService.getBookById(bookID);
+
+    }
+
+    public void getBookByTitle(){
+        System.out.println("Enter book title: ");
+        String bookName = sc.nextLine();
+
+        conService.getBookByTitle(bookName);
+
+    }
+
+    public void addBook(){
+
+        System.out.println("Add title of book:");
+        String bookName = sc.nextLine();
+
+        System.out.println("Add description of book:");
+        String description = sc.nextLine();
+
+        conService.addBook(bookName, description);
+    }
+
+    public void updateBook(){
+
+        System.out.println("To update book title and description first enter book ID: ");
+        Long bookId = sc.nextLong();
+
+        System.out.println("Update book title: ");
+        String newBookName = sc.nextLine();
+
+        System.out.println("Update description: ");
+        String newDescription = sc.nextLine();
+
+        conService.updateBook(bookId, newBookName, newDescription);
+    }
+
+    public void deleteBook(){
+
+        System.out.println("Enter the ID of the book you wish to remove: ");
+        Long bookId = sc.nextLong();
+
+        conService.deleteBook(bookId);
     }
 
 
---
+++ client/src/main/java/com/example/client/ContactService.java
@@ -0,0 +1,78 @@
+package com.example.client;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ContactService {
+
+    private RestTemplate restTemplate = new RestTemplate();
+    String project2Url = "http://localhost:8085"; //not correct
+
+    public String getAllBooks(){
+        return restTemplate.getForObject(project2Url + "/getBooks", String.class);
+    }
+
+    public String getBookById(Long bookID){
+        try {
+            return restTemplate.getForObject(project2Url + "/getBook/{bookId}", String.class);
+        }catch (HttpClientErrorException e){
+            System.out.println("Book ID: " + bookID + "\n This ID is not valid.");
+            return "";
+        }
+    }
+
+    public String getBookByTitle(String bookName){
+        try{
+            return restTemplate.getForObject(project2Url + "getBook/{bookName}", String.class);
+        }catch (HttpClientErrorException e) {
+            System.out.println("Book title: " + bookName + "\n This book title is not valid.");
+            return "";
+        }
+
+    }
+
+    public void addBook(String bookName, String description){
+        Book book = new Book();
+        book.setBookName(bookName);
+        book.setDescription(description);
+
+        HttpEntity<Book> request = new HttpEntity<>(book);
+        try{
+            restTemplate.postForObject(project2Url + "/addBook", request, Book.class);
+        }catch (Exception e){
+            System.out.println("This book " + bookName + " cannot be added.");
+        }
+    }
+
+    public void deleteBook(Long bookId) {
+        try{
+            restTemplate.delete(project2Url + "/removeBook/{bookId}" + bookId);
+        }catch(Exception e){
+            System.out.println("Book ID" + bookId + "\n could not be removed.");
+        }
+
+    }
+
+    public void updateBook(Long bookId, String newBookName, String newDescription) {
+        String bookInRepo = getBookById(bookId);
+        if (bookInRepo.equals("")) {
+            return; //the right error message (getUserById) gets displayed if this check fails as well as returning
+        }
+
+        Book book = new Book();
+        book.setBookName(newBookName);
+        book.setDescription(newDescription);
+
+        HttpEntity<Book> request = new HttpEntity<>(book);
+        try {
+            restTemplate.put(project2Url + "/updateBook/{bookName}" + newBookName, newDescription, request, Book.class);
+        } catch (Exception e) {
+            System.out.println("The book " + newBookName + " is already taken.");
+        }
+    }
+
+
+}
